cf937f8f1138da3456ae7ae8f847ab4130c08138
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2018-06-18 19:28:39 +0100

    Added base project for the new PodCaster project.

---
+++ PodCaster/eWolfPodcasterUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("eWolfPodcasterUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("eWolfPodcasterUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4be384af-0600-4182-a959-2e35e0828faf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PodCaster/eWolfPodcasterUnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace eWolfPodcasterUnitTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            int i = 0;
+            i++;
+            i.Should().Be(1);
+        }
+    }
+}
