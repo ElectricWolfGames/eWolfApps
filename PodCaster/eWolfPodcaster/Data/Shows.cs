e77a4718c1fcac44960d2c335cf7c3ae08768701
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2018-06-20 06:43:03 +0100

    Update the logic for adding new shows + Tests

---
+++ PodCaster/eWolfPodcaster/Data/Shows.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace eWolfPodcaster.Data
+{
+    public class Shows
+    {
+        private ICollection<Show> _shows = new List<Show>();
+
+        public int Count
+        {
+            get { return _shows.Count; }
+        }
+
+        public void Add(Show show)
+        {
+            if (_shows.Where((x) => x.RssFeed == show.RssFeed).Any())
+                return;
+
+            if (_shows.Where((x) => x.Title == show.Title).Any())
+                return;
+
+            _shows.Add(show);
+        }
+    }
+}
---
+++ PodCaster/eWolfPodcasterUnitTests/Data/ShowsTests.cs
@@ -0,0 +1,67 @@
+﻿using eWolfPodcaster.Data;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace eWolfPodcasterUnitTests
+{
+    public class ShowsTests
+    {
+        [Test]
+        public void ShouldAddShowToShowList()
+        {
+            Show s = new Show()
+            {
+                Title = "My Title",
+                RssFeed = "Somewhere"
+            };
+
+            Shows shows = new Shows();
+            shows.Add(s);
+
+            shows.Count.Should().Be(1);
+        }
+
+        [Test]
+        public void ShouldNotAddAnotherShowWithSameRSSFeedOrTitle()
+        {
+            Show s = new Show()
+            {
+                Title = "My Title",
+                RssFeed = "Somewhere"
+            };
+            Show sb = new Show()
+            {
+                Title = "My Title",
+                RssFeed = "elseWhere"
+            };
+
+            Shows shows = new Shows();
+            shows.Add(s);
+            shows.Add(s);
+            shows.Add(sb);
+
+            shows.Count.Should().Be(1);
+        }
+
+        [Test]
+        public void ShouldBeAbleToAddTwoDifferentShows()
+        {
+            Show s = new Show()
+            {
+                Title = "My Title A",
+                RssFeed = "Somewhere"
+            };
+            Show sb = new Show()
+            {
+                Title = "My Title B",
+                RssFeed = "elseWhere"
+            };
+
+            Shows shows = new Shows();
+            shows.Add(s);
+            shows.Add(sb);
+
+            shows.Count.Should().Be(2);
+        }
+    }
+}
