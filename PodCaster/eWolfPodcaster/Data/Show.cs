3077eaccf4be53df10d779a683bdfde81a2e2484
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2018-06-20 06:24:25 +0100

    Updated data store objects

--- PodCaster/eWolfPodcaster/Data/Show.cs
+++ PodCaster/eWolfPodcaster/Data/Show.cs
@@ -4,18 +4,12 @@ namespace eWolfPodcaster.Data
 {
     public class Show
     {
-        public string Category { get; set; }
-
-        public bool CheckforUpdates { get; set; }
-
-        public bool IncludeSubFolders { get; set; }
+        public ICollection<Episode> Episodes { get; set; }
 
         public string RssFeed { get; set; }
 
-        public ShowStorageType ShowStorage { get; set; }
+        public ShowOptions ShowOption { get; set; } = new ShowOptions();
 
         public string Title { get; set; }
-
-        public ICollection<Episode> Episodes { get; set; }
     }
 }
---
+++ PodCaster/eWolfPodcaster/Data/ShowOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace eWolfPodcaster.Data
+{
+    public class ShowOptions
+    {
+        public bool AudoDownloadEpisodes { get; set; }
+
+        public string Category { get; set; }
+
+        public bool CheckforUpdates { get; set; }
+
+        public bool IncludeSubFolders { get; set; }
+
+        public ShowStorageType ShowStorage { get; set; }
+    }
+}
