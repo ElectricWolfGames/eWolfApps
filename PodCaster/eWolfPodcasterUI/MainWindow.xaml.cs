f5ebe7a7a759061ca2219afd56fdcab89579e38b
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2018-07-11 19:59:16 +0100

    Added basic play controls

--- PodCaster/eWolfPodcasterUI/MainWindow.xaml.cs
+++ PodCaster/eWolfPodcasterUI/MainWindow.xaml.cs
@@ -4,20 +4,25 @@ using eWolfPodcasterUI.CustomDialog;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace eWolfPodcasterUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        private Shows _shows = new Shows();
+        private readonly MediaPlayer _mediaPlayer = new MediaPlayer();
+        private PodcastEpisode _currentPodcast = null;
         private ObservableCollection<IPodCastInfo> _podcasts = new ObservableCollection<IPodCastInfo>();
+        private Shows _shows = new Shows();
 
         public MainWindow()
         {
@@ -28,6 +33,29 @@ namespace eWolfPodcasterUI
             _shows.Save(GetOutputFolder());
 
             AddShowItems();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Tick += TimerTick;
+            timer.Start();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string PodcastDescription
+        {
+            get
+            {
+                return _currentPodcast?.Description;
+            }
+        }
+
+        public string PodcastName
+        {
+            get
+            {
+                return _currentPodcast?.Title;
+            }
         }
 
         public string GetOutputFolder()
@@ -35,9 +63,33 @@ namespace eWolfPodcasterUI
             return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "eWolf\\eWolfTestApp");
         }
 
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
         private void AddShowItems()
         {
             ShowsItems.ItemsSource = _shows.ShowList;
+        }
+
+        private void BtnPause_Click(object sender, RoutedEventArgs e)
+        {
+            _mediaPlayer.Pause();
+        }
+
+        private void BtnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            _mediaPlayer.Play();
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+            _mediaPlayer.Stop();
         }
 
         private void ButtonAddShowClick(object sender, RoutedEventArgs e)
@@ -67,15 +119,27 @@ namespace eWolfPodcasterUI
             _shows.RemoveShow(selectedItem);
         }
 
-        private void EpisodesItems_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void EpisodeListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var item = (sender as ListView).SelectedItem;
-            EpisodeControl ec = item as EpisodeControl;
+            if (e.AddedItems.Count == 0)
+                return;
 
-            if (ec != null)
+            _currentPodcast = e.AddedItems[0] as PodcastEpisode;
+
+            string urlToPlay = string.Empty;
+            if (_currentPodcast.IsOffLine())
             {
-                // show this as the episode to play.
+                urlToPlay = _currentPodcast.GetOffLineFileName();
             }
+            else
+            {
+                urlToPlay = _currentPodcast.PodcastURL.Replace("https", "http");
+            }
+
+            _mediaPlayer.Open(new Uri(urlToPlay));
+            _mediaPlayer.Position = new TimeSpan(_currentPodcast.PlayedLength);
+            _mediaPlayer.Play();
+            OnPropertyChanged("PodcastDescription");
         }
 
         private void ShowsItems_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -101,6 +165,20 @@ namespace eWolfPodcasterUI
                 }
 
                 EpisodesItems.ItemsSource = _podcasts;
+            }
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            if (_mediaPlayer.Source != null && _mediaPlayer.NaturalDuration.HasTimeSpan)
+            {
+                _currentPodcast.PlayedLength = _mediaPlayer.Position.Ticks;
+
+                double totalWidth = 781;
+
+                totalWidth /= _mediaPlayer.NaturalDuration.TimeSpan.TotalMilliseconds;
+                totalWidth *= _mediaPlayer.Position.TotalMilliseconds;
+                _currentPodcast.PlayedLengthScaled = (float)totalWidth;
             }
         }
     }
