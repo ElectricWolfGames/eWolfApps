453da8806214603f19b0b30bf380c45f6e01da17
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2018-07-14 09:10:35 +0100

    Updated UWP layout and add new show page(WIP)

--- PodCaster/eWolfPodcasterUWP/MainPage.xaml.cs
+++ PodCaster/eWolfPodcasterUWP/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using eWolfPodcasterCore.Data;
+using eWolfPodcasterUWP.Pages;
 using System;
 using System.IO;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace eWolfPodcasterUWP
@@ -11,7 +13,11 @@ namespace eWolfPodcasterUWP
 
         public MainPage()
         {
+            Console.WriteLine("ctor");
+
             this.InitializeComponent();
+
+            this.NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
 
             Uri newuri = new Uri("http://media.blubrry.com/codingblocks/s/www.podtrac.com/pts/redirect.mp3/traffic.libsyn.com/codingblocks/coding-blocks-episode-84.mp3");
             // myPlayer.Source = newuri;
@@ -32,6 +38,35 @@ namespace eWolfPodcasterUWP
         private void AddShowItems()
         {
             ShowsItems.ItemsSource = _shows.ShowList;
+        }
+
+        private void ButtonAddShowClick(object sender, RoutedEventArgs e)
+        {
+            /*AddNewShow addNewShow = new AddNewShow();
+            addNewShow.ShowName = "new show name";
+            addNewShow.ShowDialog();
+
+            if (addNewShow.Apply)
+            {
+                ShowControl sc = new ShowControl()
+                {
+                    Title = addNewShow.ShowName,
+                    RssFeed = addNewShow.RSSFeed
+                };
+                _shows.Add(sc);
+            }*/
+
+            this.Frame.Navigate(typeof(AddNewShow));
+        }
+
+        private void ButtonSubShowClick(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = ShowsItems.SelectedItems;
+            if (selectedItems.Count == 0)
+                return;
+
+            ShowControl selectedItem = (ShowControl)selectedItems[0];
+            _shows.RemoveShow(selectedItem);
         }
     }
 }
---
+++ PodCaster/eWolfPodcasterUWP/Pages/AddNewShow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace eWolfPodcasterUWP.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AddNewShow : Page
+    {
+        private string _showName = string.Empty;
+        private string _rssFeed = string.Empty;
+
+        public AddNewShow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool Apply { get; set; }
+
+        public string RSSFeed
+        {
+            get
+            {
+                return _rssFeed;
+            }
+            set
+            {
+                _rssFeed = value;
+                OnPropertyChanged("RSSFeed");
+            }
+        }
+
+        public string ShowName
+        {
+            get
+            {
+                return _showName;
+            }
+
+            set
+            {
+                _showName = value;
+                OnPropertyChanged("ShowName");
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private void buttonOKClick(object sender, RoutedEventArgs e)
+        {
+            Apply = false;
+            ClosePage();
+        }
+
+        private void buttonCancelClick(object sender, RoutedEventArgs e)
+        {
+            Apply = true;
+            ClosePage();
+        }
+
+        private void ClosePage()
+        {
+            this.Frame.GoBack();
+        }
+    }
+}
