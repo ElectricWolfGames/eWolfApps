3c655e3b251ef5dfb4990c6eadb656e4cf326738
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2018-09-17 19:43:11 +0100

    added ServiceLocatorTests re-added DateTimeHelper

--- PodCaster/eWolfPodcasterUnitTests/Helpers/DataHelperTests.cs
+++ PodCaster/eWolfPodcasterUnitTests/Helpers/DataHelperTests.cs
@@ -8,7 +8,7 @@ namespace eWolfPodcasterCoreUnitTests.Helpers
 {
     public class DataHelperTests
     {
-        [Ignore("Another problem with datetime conversion")]
+        //[Ignore("Another problem with datetime conversion")]
         [TestCase("Sat, 31 Dec 2016 00:08:57 +0000", "31/12/2016 00:08:57")]
         [TestCase("Sun, 08 Jan 2017 06:47:50 GMT", "08/01/2017 06:47:50")]
         [TestCase("Tue, 18 May 2015 18:00:01 GMT", "18/05/2015 18:00:01")]
