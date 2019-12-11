c5390bf5ca6d916eb97b57a5cafe6218264aecbd
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2019-12-11 18:17:19 +0000

    Added FileHelper - Create back-up of files

---
+++ Common/eWolfCommon/Helpers/FileHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace eWolfCommon.Helpers
+{
+    public static class FileHelper
+    {
+        public static bool CreateBackUp(string folder, string filename)
+        {
+            string oldFileName = $"{folder}\\{filename}";
+
+            if (File.Exists(oldFileName))
+            {
+                string newFileName = $"{folder}\\_backup_{filename}";
+                File.Delete(newFileName);
+                File.Move(oldFileName, newFileName);
+                return true;
+            }
+            return false;
+        }
+    }
+}
