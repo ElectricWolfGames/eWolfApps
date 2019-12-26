62853479d7fd3ff066451c13e3d85e3dd5a0dfe1
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2019-12-26 10:03:41 +0000

    Added basic unit tests (don't add any value at this time)

---
+++ SystemTray/SystemTrayToolsUnitTests/TakeScreenGrabTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SystemTrayTools.Actions;
+
+namespace SystemTrayToolsUnitTests
+{
+    public class TakeScreenGrabTests
+    {
+        [Test]
+        public void ShouldGetOrder()
+        {
+            TakeScreenGrab tsg = new TakeScreenGrab();
+            tsg.OrderIndex.Should().Be(10);
+        }
+    }
+}
