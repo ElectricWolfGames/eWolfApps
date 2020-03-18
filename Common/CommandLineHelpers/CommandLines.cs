bc067d7ad63f1d90aab203f7035cd7e050d14fc1
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2020-03-18 17:10:15 +0000

    Added CommandLineHelpers

---
+++ Common/CommandLineHelpers/CommandLines.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CommandLineHelpers
+{
+    public class CommandLines
+    {
+        private const int LF_FACESIZE = 32;
+
+        private const int STD_OUTPUT_HANDLE = -11;
+
+        private const int TMPF_TRUETYPE = 4;
+
+        private static IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);
+
+        public static void StartWindow()
+        {
+            AllocConsole();
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool AllocConsole();
+
+        public static void SetFont(string fontName = "Lucida Console", int fontSize = 700)
+        {
+            unsafe
+            {
+                IntPtr hnd = GetStdHandle(STD_OUTPUT_HANDLE);
+                if (hnd != INVALID_HANDLE_VALUE)
+                {
+                    // Set console font to Lucida Console.
+                    CONSOLE_FONT_INFO_EX newInfo = new CONSOLE_FONT_INFO_EX();
+                    newInfo.cbSize = (uint)Marshal.SizeOf(newInfo);
+                    newInfo.FontFamily = TMPF_TRUETYPE;
+                    IntPtr ptr = new IntPtr(newInfo.FaceName);
+                    Marshal.Copy(fontName.ToCharArray(), 0, ptr, fontName.Length);
+
+                    // Get some settings from current font.
+
+                    newInfo.dwFontSize = new COORD(newInfo.dwFontSize.X, newInfo.dwFontSize.Y);
+                    newInfo.FontWeight = fontSize;
+                    SetCurrentConsoleFontEx(hnd, false, ref newInfo);
+                }
+            }
+        }
+
+        public static void SetFontColor(ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+        }
+
+        public static void SetWindowSize(int width, int height)
+        {
+            Console.SetWindowSize(width, height);
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetStdHandle(int dwType);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern int SetConsoleFont(
+            IntPtr hOut,
+            uint dwFontNum
+            );
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool SetCurrentConsoleFontEx(
+            IntPtr consoleOutput,
+            bool maximumWindow,
+            ref CONSOLE_FONT_INFO_EX consoleCurrentFontEx);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        internal unsafe struct CONSOLE_FONT_INFO_EX
+        {
+            internal uint cbSize;
+            internal uint nFont;
+            internal COORD dwFontSize;
+            internal int FontFamily;
+            internal int FontWeight;
+            internal fixed char FaceName[LF_FACESIZE];
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct COORD
+        {
+            internal short X;
+            internal short Y;
+
+            internal COORD(short x, short y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+    }
+}
---
+++ Common/CommandLineHelpers/CommandRunner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CommandLineHelpers
+{
+    public static class CommandRunner
+    {
+        public static void ExecuteCommand(string command)
+        {
+            Console.WriteLine($"And the command is {command}");
+
+            ProcessStartInfo ProcessInfo;
+            Process Process;
+
+            ProcessInfo = new ProcessStartInfo("cmd.exe", "/C " + command);
+            ProcessInfo.WorkingDirectory = @"C:\Git\";
+            ProcessInfo.CreateNoWindow = true;
+            ProcessInfo.UseShellExecute = true;
+            ProcessInfo.Verb = "runas";
+            Process = Process.Start(ProcessInfo);
+        }
+    }
+}
