6130dc9a9d338c4d1479c479c39ebc98d7801825
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2020-03-18 17:09:42 +0000

    Added TextHelper to common

---
+++ Common/eWolfCommon/Helpers/TextHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace eWolfCommon.Helpers
+{
+    public static class TextHelper
+    {
+        public static string ToSentenceCase(string word)
+        {
+            return Regex.Replace(word, "[a-z][A-Z]", m => $"{m.Value[0]} {char.ToLower(m.Value[1])}");
+        }
+    }
+}
---
+++ Common/eWolfCommonUnitTests/Helpers/TextHelperTests.cs
@@ -0,0 +1,18 @@
+﻿using eWolfCommon.Helpers;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace eWolfCommonUnitTests.Helpers
+{
+    public class TextHelperTests
+    {
+        [TestCase("Text", "Text")]
+        [TestCase("TextMoreWords", "Text more words")]
+        [TestCase("TextMore Words", "Text more Words")]
+        public void ShouldConvertToSentenceCase(string from, string to)
+        {
+            string text = TextHelper.ToSentenceCase(from);
+            text.Should().Be(to);
+        }
+    }
+}
