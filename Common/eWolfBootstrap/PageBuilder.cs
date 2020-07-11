8940aaeca7d26f3c4f456e86fed9ea3c9736e2a1
ElectricWolfGames <39939684+ElectricWolfGames@users.noreply.github.com>
2020-07-11 08:49:46 +0100

    Added Bootstrap helper project

---
+++ Common/eWolfBootstrap/PageBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace eWolfBootstrap
+{
+    public class PageBuilder
+    {
+        private StringBuilder _stringBuilder = new StringBuilder();
+        private string _fileName;
+        private string _path;
+
+        public PageBuilder(string fileName, string path)
+        {
+            _fileName = fileName;
+            _path = path;
+        }
+
+        public void Append(string text)
+        {
+            _stringBuilder.Append(text);
+        }
+
+        public void Output()
+        {
+            File.WriteAllText(_path + _fileName, _stringBuilder.ToString());
+        }
+    }
+}
