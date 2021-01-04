2b73f18d69bf645e93c1cdad788bf2376028f3c4
Karl Gillott <karlgillott@GoogleMail.com>
2021-01-04 07:32:23 +0000

    Added GetDriveLetterFor to get the drive letter of a volumne

--- Common/eWolfCommon/Helpers/FileHelper.cs
+++ Common/eWolfCommon/Helpers/FileHelper.cs
@@ -5,20 +5,6 @@ namespace eWolfCommon.Helpers
 {
     public static class FileHelper
     {
-        public static bool CreateBackUp(string folder, string filename)
-        {
-            string oldFileName = $"{folder}\\{filename}";
-
-            if (File.Exists(oldFileName))
-            {
-                string newFileName = $"{folder}\\_backup_{filename}";
-                File.Delete(newFileName);
-                File.Move(oldFileName, newFileName);
-                return true;
-            }
-            return false;
-        }
-
         public static bool CreateBackFileDate(string folder, string filename)
         {
             string oldFileName = $"{folder}\\{filename}";
@@ -33,6 +19,39 @@ namespace eWolfCommon.Helpers
                 return true;
             }
             return false;
+        }
+
+        public static bool CreateBackUp(string folder, string filename)
+        {
+            string oldFileName = $"{folder}\\{filename}";
+
+            if (File.Exists(oldFileName))
+            {
+                string newFileName = $"{folder}\\_backup_{filename}";
+                File.Delete(newFileName);
+                File.Move(oldFileName, newFileName);
+                return true;
+            }
+            return false;
+        }
+
+        public static string GetDriveLetterFor(string name)
+        {
+            name = name.ToLower();
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (var drive in drives)
+            {
+                if (drive.IsReady)
+                {
+                    if (drive.VolumeLabel.ToLower() == name)
+                    {
+                        return drive.Name;
+                    }
+                }
+            }
+            return null;
         }
     }
 }
